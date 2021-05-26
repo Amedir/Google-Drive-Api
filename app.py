e768152fc87a275ace05c764c3980b71f79f2bf2
Amedir <ademirmsfilho@fac.pe.senac.br>
2021-05-26 19:49:54 -0300

    Adciona nova URI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, session, abort
 from Google import Create_Service
 from flask_restful import Resource, Api
 
@@ -11,13 +11,18 @@ service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 app = Flask(__name__)
 api = Api(app)
 
+
 # Lista todas as pastas (processos) a partir do folder_id
 class ListarPastas(Resource):
     def get(self):
         folder_id = '1TSY3KWrVYZQY2SdTivbYgI1VdFjnoO4O'
         query = f"parents = '{folder_id}' and trashed=false and mimeType = 'application/vnd.google-apps.folder'"
 
-        response = service.files().list(q = query, fields='nextPageToken, files(id, name, mimeType, webViewLink, iconLink)').execute()
+        response = service.files().list(
+            q = query,
+            fields = 'nextPageToken, files(id, name, mimeType, webViewLink, iconLink, description)'
+        ).execute()
+
         files = response.get('files')
         nextPageToken = response.get('nextPageToken')
 
@@ -26,7 +31,9 @@ class ListarPastas(Resource):
             files.extend(response.get('files'))
             nextPageToken = response.get('nextPageToken')
 
-        return response
+        print(files[0])
+        return files
+
 
 # lista todos os arquivos ou pastas a partir da pasta selecionada
 class SelecionaPastas(Resource):
@@ -34,8 +41,10 @@ class SelecionaPastas(Resource):
         folder_id = id
         query = f"parents = '{folder_id}' and trashed=false and mimeType != 'application/vnd.google-apps.folder'"
 
-        response = service.files().list(q = query,
-                                        fields = 'nextPageToken, files(id, name, mimeType, webViewLink, iconLink)').execute()
+        response = service.files().list(
+            q = query,
+            fields = 'nextPageToken, files(id, name, mimeType, webViewLink, iconLink)'
+        ).execute()
         files = response.get('files')
         nextPageToken = response.get('nextPageToken')
 
@@ -46,13 +55,14 @@ class SelecionaPastas(Resource):
 
         return response
 
+
 class Pesquisa(Resource):
     def get(self, nome):
         page_token = None
         while True:
             response = service.files().list(
-                q = "fullText contains '" + nome + "' and mimeType = 'application/vnd.google-apps.folder'",
-                fields = 'nextPageToken, files(id, name, mimeType, webViewLink, iconLink, viewedByMeTime)',
+                q = f"fullText contains '{nome}' and mimeType = 'application/vnd.google-apps.folder' and trashed=false",
+                fields = 'nextPageToken, files(id, name, mimeType, webViewLink, iconLink)',
                 pageToken = page_token
             ).execute()
             for file in response.get('files', []):
@@ -62,8 +72,10 @@ class Pesquisa(Resource):
 
         return response
 
+
+
 api.add_resource(ListarPastas, '/')
-api.add_resource(SelecionaPastas, '/<string:id>/')
+api.add_resource(SelecionaPastas, '/teste/<string:id>')
 api.add_resource(Pesquisa, '/pesquisa/<string:nome>')
 
 # /
@@ -73,4 +85,4 @@ api.add_resource(Pesquisa, '/pesquisa/<string:nome>')
 # /pesquisa/
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
