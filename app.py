178d4b6d01c7c4ecb2f902883502bc69c1cea957
Amedir <ademirmsfilho@fac.pe.senac.br>
2021-05-28 17:11:32 -0300

    Corrige URI

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from pip._vendor import cachecontrol
 from google.oauth2 import id_token
 import google.auth.transport.requests
 import os
-import pathlib
 import requests
 
 CLIENT_SECRET_FILE = 'Client_Secret.json'
@@ -29,10 +28,9 @@ app.secret_key = b'8q;\xc8Y.\xc0\x7f'
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 GOOGLE_CLIENT_ID = "470181059605-uiii7hhq17aml3efcg1f7jr4da3tmkvh.apps.googleusercontent.com"
-client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "Client_Secret.json")
 
 flow = Flow.from_client_secrets_file(
-    client_secrets_file = client_secrets_file,
+    client_secrets_file = CLIENT_SECRET_FILE,
     scopes = ["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email",
               "openid"],
     redirect_uri = "http://127.0.0.1:5000/callback"
